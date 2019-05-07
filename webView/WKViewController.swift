aa5ba2f0b1334b979e7d1566cfc22204ce444f77
Alice <achang@health2sync.com>
2019-05-07 15:23:25 +0800

    Add forwardBtn and backBtn

--- webView/WKViewController.swift
+++ webView/WKViewController.swift
@@ -10,17 +10,48 @@
 import UIKit
 import WebKit
 
-class WKViewController: UIViewController, WKUIDelegate {
+class WKViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
     
+    @IBOutlet weak var forwardBtn: UIButton!
+    @IBOutlet weak var backBtn: UIButton!
     @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let webConfiguration = WKWebViewConfiguration()
+        
+        webConfiguration.accessibilityPerformEscape()
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
         
         if let url = URL(string: "https://en.wikipedia.org/wiki/Swift_(programming_language)") {
             let requset = URLRequest(url: url)
             webView.load(requset)
         }
         
+        
+        
     }
-  
+    
+    //自定義上下頁按鈕
+    @IBAction func backBtn(_ sender: Any) {
+
+        if webView.canGoBack{
+            webView.goBack()
+        }
+        
+        
+    }
+    
+    @IBAction func forwardBtn(_ sender: Any) {
+        if webView.canGoForward{
+            webView.goForward()
+        }
+    }
+    
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        backBtn.isEnabled = webView.canGoBack
+        forwardBtn.isEnabled = webView.canGoForward
+    }
+    
 }
